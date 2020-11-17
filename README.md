689ae950dd745c19f925dad0a84b81928e5779cf
imankit01 <ankitkumar2593@gmail.com>
2020-11-17 17:58:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# AirNow - Real-Time Air Quality Index Web App
+An Air Quality App Built With React.JS
+
+## Introduce
+This project is aiming at providing a instant air quality index to the users based on their geolocation. 
+
+It is using [create-react-app](https://github.com/facebook/create-react-app) for development.
+
+## Usage
+Visit -> [https://airnow.dawei.io/](https://airnow.dawei.io/)
+
+or
+
+``` shell
+# install dependencies
+npm i 
+# run app
+npm start
+# build app
+npm build
+```
+
+## API
+Backend API powered by [The World Air Quality Index (AQI) Project](http://waqi.info/)
---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import DisplayBoard from './component/displayBoard';
+import { getNewAirData } from './actions/index';
+import Loader from './component/loader';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { location: null, counting:true };
+    this.checkNowOnClick = this.checkNowOnClick.bind(this);
+    this.getCountingFinished = this.getCountingFinished.bind(this);
+  }
+  componentWillMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({ location: position });
+        this.props.ge(this.state.location.coords);
+      });
+    }
+    // console.log('nothing');
+  }
+  // componentDidMount() {
+  //   let height = window.outerHeight.toString();
+    
+  //   document.querySelector("body").style.height = height+"px"
+  // }
+  checkNowOnClick(e) {
+    if (navigator.geolocation) {
+      this.setState({ location: null, counting: true });
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({ location: position });
+        this.props.ge(this.state.location.coords);
+      });
+    }
+  }
+  getCountingFinished(flag) {
+    if (flag){
+      this.setState({counting: false});
+    }
+  }
+  render() {
+      if (this.state.location) {
+        // const location = this.state.location.coords;
+        // console.log(location)
+        return (
+          <div>
+            <DisplayBoard getCountingFinished={this.getCountingFinished} />
+            {this.state.counting  
+            ? <button onClick={this.checkNowOnClick} className="check-button diable" disabled>Checking</button>
+            : <button onClick={this.checkNowOnClick} className="check-button">Update</button>
+            }
+            <div className={`global-bg ${this.props.background}`}></div>
+          </div>
+        );
+      }
+    return (
+      <div className="vertical-center"> 
+        <div className="loader-container">
+          <Loader />
+          <p className="text-center">
+            The app will fetch the air quality index based on your location. 
+          </p>
+          <p className="text-center">
+            <strong>Make sure your Location Service is turned on.</strong>
+          </p>
+      </div>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({ ge: getNewAirData }, dispatch);
+
+const mapStateToProps = (state) => {
+  return {
+    background: state.indexLevel.background
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
